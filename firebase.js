78d3a2cf473b690382a3c499a147fc74e1830df2
ittzsarias-brand <ittz.sarias@gmail.com>
2026-09-04 15:17:25 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,15 +1,17 @@
+// firebase.js
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-auth.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-storage.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB6tzEGVTqNi1rCkPHKzFEK6fFwNhJU1Xw",
-    authDomain: "sura-store-5511b.firebaseapp.com",
-    projectId: "sura-store-5511b",
-    storageBucket: "sura-store-5511b.firebasestorage.app",
-    messagingSenderId: "859109347846",
-    appId: "1:859109347846:web:16abe6feb56036144de987"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "sura-store-5511b.firebaseapp.com",
+  projectId: "sura-store-5511b",
+  storageBucket: "sura-store-5511b.firebasestorage.app",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -18,9 +20,4 @@ const db = getFirestore(app);
 const auth = getAuth(app);
 const storage = getStorage(app);
 
-export {
-    app,
-    db,
-    auth,
-    storage
-};
+export { app, db, auth, storage };
